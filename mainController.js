3bf6c2f4b03e393d80d142c051ef8b94f251e2b8
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2021-05-25 22:21:32 +0900

    환경 테스트

--- mainController.js
+++ mainController.js
@@ -1,9 +1,9 @@
 const passwordSecurityClient = require('./PasswordSecurityClient');
 const request = require('request');
 
-// request.get(
-//   {url:'http://3.83.136.237:65001/test'},
-//   function(err, res, body) {
-//     console.log(body);
-//   }
-// );
+request.get(
+  {url:'http://localhost:65001/Test'},
+  function(err, res, body) {
+    console.log(body);
+  }
+);
