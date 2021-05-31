34df859bce92412e73348fb6db051b615d55867e
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2021-05-31 23:28:35 +0900

    Update mainController.js

--- mainController.js
+++ mainController.js
@@ -2,7 +2,7 @@ const passwordSecurityClient = require('./PasswordSecurityClient');
 const request = require('request');
 
 request.get(
-  {url:'http://localhost:65001/Test'},
+  {url:'http://192.168.0.18:65001/Test'},
   function(err, res, body) {
     console.log(body);
   }
