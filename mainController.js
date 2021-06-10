92b0bdcee38da9760daf891b9a2538b6b8538af6
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-11 00:35:39 +0900

    npm study

--- mainController.js
+++ mainController.js
@@ -1,9 +1,11 @@
 const passwordSecurityClient = require('./PasswordSecurityClient');
 const request = require('request');
 
-request.get(
-  {url:'http://54.157.166.99:65001/Test'},
-  function(err, res, body) {
-    console.log(body);
-  }
-);
+// request.get(
+//   {url:'http://54.157.166.99:65001/Test'},
+//   function(err, res, body) {
+//     console.log(body);
+//   }
+// );
+
+console.log(passwordSecurityClient.passwordSecurityClient.test());
