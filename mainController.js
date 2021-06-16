b52cdbd73e2982f89549f00915fd3b3589d39e3f
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-16 16:50:12 +0900

    file download test

--- mainController.js
+++ mainController.js
@@ -1,6 +1,8 @@
 // const passwordSecurityClient = require('./PasswordSecurityClient');
 const passwordSecurityClient = require('@kihyeon-hong/passwordsecurityclient');
 const request = require('request');
+const fs = require('fs');
+const http = require('http');
 
 // request.get(
 //   {url:'http://54.157.166.99:65001/Test'},
@@ -9,4 +11,17 @@ const request = require('request');
 //   }
 // );
 
-console.log(passwordSecurityClient.passwordSecurityClient.test());
+// request.get(
+//   {url:'http://localhost:65001/download'},
+//   function(err, res, body) {
+//     fs.writeFileSync('./weights.bin', body);
+//   }
+// );
+
+const file = fs.createWriteStream("weights.bin");
+http.get("http://localhost:65001/download", function(response) {
+  response.pipe(file);
+});
+
+
+// console.log(passwordSecurityClient.passwordSecurityClient.test());
