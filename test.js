6194a9efbf1d20e9c895c8db6e55c0bab911020b
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-21 19:27:14 +0900

    AIoT 개발 시작

---
+++ test.js
@@ -0,0 +1,10 @@
+const PasswordSecurityClient = require('./PasswordSecurityClient');
+
+var pwd = new PasswordSecurityClient.passwordSecurity.passwordSecurity();
+
+
+console.log(pwd.passwordValidation());
+console.log(pwd.passwordModelDistribution());
+console.log(pwd.passwordDictUpdate());
+console.log(pwd.passwordModelParaUpdate());
+console.log(pwd.getLog());
