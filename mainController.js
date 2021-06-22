783deff9aec1db4521ba7714af9feda002fd3cd6
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-22 15:29:50 +0900

    test code

--- mainController.js
+++ mainController.js
@@ -4,24 +4,3 @@ const request = require('request');
 const fs = require('fs');
 const http = require('http');
 
-// request.get(
-//   {url:'http://54.157.166.99:65001/Test'},
-//   function(err, res, body) {
-//     console.log(body);
-//   }
-// );
-
-// request.get(
-//   {url:'http://localhost:65001/download'},
-//   function(err, res, body) {
-//     fs.writeFileSync('./weights.bin', body);
-//   }
-// );
-
-// const file = fs.createWriteStream("weights.bin");
-// http.get("http://localhost:65001/download", function(response) {
-//   response.pipe(file);
-// });
-
-
-// console.log(passwordSecurityClient.passwordSecurityClient.test());
--- test.js
+++ test.js
@@ -1,33 +1,5 @@
-const request = require('request');
 const PasswordSecurityClient = require('./PasswordSecurityClient');
 
 var pwd = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
 
-/*
-console.log(pwd.passwordValidation());
-console.log(pwd.passwordModelDistribution());
-console.log(pwd.passwordDictUpdate());
-console.log(pwd.passwordModelParaUpdate());
-console.log(pwd.getLog());
-*/
-
-// request.post({
-//     headers: {
-//         'content-type': 'application/json'
-//     },
-//     url:'http://localhost:65001/passwordDictUpdate',
-//     body: {
-//         dict: {
-//             insert: "insert data",
-//             delete: "delete data"
-//         },
-//         comment: "Test Comment"
-//     },
-//     json: true
-// }, function(err, res, body) {
-//     // res.json(body);
-//     console.log(body);
-// });
-
-// console.log(pwd.passwordDictUpdate('{"insert":"insert data","delete":"delete data"}', "test comment"));
-console.log(pwd.passwordModelParaUpdate('{"node":3,"hidden":[5,5,5,5]}', "test comment"));
+pwd.passwordValidationTest();
