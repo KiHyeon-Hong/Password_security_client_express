1203461367ab3e791b5159c184262eedeef8fc37
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-23 15:27:04 +0900

    비동기 test

--- test.js
+++ test.js
@@ -2,4 +2,8 @@ const PasswordSecurityClient = require('./PasswordSecurityClient');
 
 var pwd = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
 
-pwd.passwordValidationTest();
+// pwd.passwordValidationTest("pds$66wo@d").then(function(result) {
+//     console.log(result);
+// });
+
+console.log(pwd.passwordValidationTest("pds$66wo@d"));
