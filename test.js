3b0efd0393061bd51f7e8708861362d0fff28032
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-28 16:10:58 +0900

    20210628 commit

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ const PasswordSecurityClient = require('./PasswordSecurityClient');
 
 var pwd = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
 
-// pwd.passwordValidationTest("pds$66wo@d").then(function(result) {
-//     console.log(result);
+pwd.passwordValidation("pds$66wo@d").then(function(result) {
+    console.log(result);
 
-//     pwd.passwordValidationTest("q1w2e3r4").then(function(result) {
-//         console.log(result);
-//     });    
-// });
+    pwd.passwordValidation("abcdefg").then(function(result) {
+        console.log(result);
+    });    
+});
 
 // pwd.passwordDictUpdate('{"dictionary":"q1w2e3r4"}', "test message");
 
-pwd.passwordModelParaUpdate('{"node":4,"unit":[3,5,3,1],"activation":"sigmoid","epoch":10}', "test message");
+// pwd.passwordModelParaUpdate('{"node":4,"unit":[3,5,3,1],"activation":"sigmoid","epoch":10}', "test message");
 
