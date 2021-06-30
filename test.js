a5e9095bbe0a0b496a12c26fe97080f840172fca
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-30 11:40:06 +0900

    Update Error and Test
    Update Error and Test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const PasswordSecurityClient = require('./PasswordSecurityClient');
+// const PasswordSecurityClient = require('@kihyeon-hong/password_security_client');
 
 var pwd = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
 
@@ -12,5 +13,6 @@ pwd.passwordValidation("pds$66wo@d").then(function(result) {
 
 // pwd.passwordDictUpdate('{"dictionary":"q1w2e3r4"}', "test message");
 
-// pwd.passwordModelParaUpdate('{"node":4,"unit":[3,5,3,1],"activation":"sigmoid","epoch":10}', "test message");
+// pwd.passwordModelParaUpdate('{"node":4,"unit":[3,5,3,1],"activation":"relu","epoch":7}', "test message.");
 
+// pwd.passwordDictUpdate('{"dictionary":"q1w2e3r4"}', "test message");
